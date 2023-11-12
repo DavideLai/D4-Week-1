aef11b14d69ba15c44e53ecb4fc856028a4a7cbe
DavideLai <davidelai21@yahoo.it>
2023-11-12 22:42:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+/* ESERCIZIO 1
+     Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+    */
+    
+    /*  I principali tipi di dati in JavaScript sono sei:
+
+    1. Stringhe: sono un insieme di caratteri in sequenza che possono essere numeri o lettere,
+     racchiusi da doppi apici (virgolette), apici semplici (apostrofi), o apici obliqui.
+    2. Boolean: E' un tipo di risposta a una domanda che può essere vera o falsa.
+    3. Numeri interi: può essere un qualsiasi numero tra -(2 elevato 53 -1) e +(2 elevato 53 - 1), 
+    quindi quasi tutti i numeri esistenti, purchè siano interi.
+    4. Numeri decimali: può essere un qualsiasi numero tra -(2 elevato 53 -1) e +(2 elevato 53 - 1), 
+    quindi quasi tutti i numeri esistenti, purchè abbiano la virgola. 
+    5. Undefined: Rappresenta un valore che non è stato assegnato.
+    6. Null: rappresenta un valore che è stato assegnato ma che è nullo.  */
+    
+    /* ESERCIZIO 2
+     Descrivi cos'è un oggetto in JavaScript, con parole tue.
+    */
+    
+    /* Gli oggetti in Javascript possono essere pensati come un gruppo di elementi variabili che descrivono 
+    un unico oggetto. */
+    
+    /* ESERCIZIO 3
+     Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+    */
+    
+     /* 12+20
+    
+     /* ESERCIZIO 4
+      Crea una variabile di nome "x" e assegna ad essa il numero 12.
+     */
+     
+     /* let x = 12 
+     
+     /* ESERCIZIO 5
+      Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+     */
+     
+     /* let name = "Davide"
+     
+     /* ESERCIZIO 6
+      Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+     */
+     
+     /* 4 - x = -8
+ 
+     /* ESERCIZIO 7
+     Crea un oggetto in Javascript che ti rappresenti (come proprietà usa nome, cognome e hobby) 
+     */
+ 
+     /* let davideLai {
+         nome: "Davide",
+         cognome: "Lai",
+         hobby: "Viaggiare"
+     }
+     
+     /* ESERCIZIO 8
+      Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+      Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+      Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+      NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+     */
+     
+        let name1 = "john"
+        let name2 = "John"
+      
+      console.log("John"==="john")
+      console.log("john"==="john")
